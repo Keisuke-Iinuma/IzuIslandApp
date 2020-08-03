a9e378244aba42028f2e647dfb973d0b85590727
Keisuke <atcq2003@hotmail.com>
2020-08-03 22:17:17 +0900

    Initial Commit

---
+++ IzuIslandApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IzuIslandApp
+//
+//  Created by 飯沼圭哉 on 2020/08/03.
+//  Copyright © 2020 keisuke.iinuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
